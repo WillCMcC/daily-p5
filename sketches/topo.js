cf10aa9145d629e4d31fe59acc8481d405c55ded
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2022-03-05 13:25:30 -0800

    resolution

--- sketches/topo.js
+++ sketches/topo.js
@@ -22,10 +22,10 @@ const Sketch = (W, H) => (p) => {
     second,
     set,
   } = p;
-  const maxHeight = 100;
-  const heightMarker = 5;
+  const maxHeight = 1000;
+  const heightMarker = 50;
   const tf = new (Transformer(p))();
-
+  const resolution = Math.random() * 100;
   p.setup = () => {
     p.createCanvas(W, H);
     p.angleMode(RADIANS);
@@ -33,12 +33,11 @@ const Sketch = (W, H) => (p) => {
     let white = p.color(255);
     for (let x = 0; x < W; x++) {
       for (let y = 0; y < H; y++) {
-        const v = p.noise(0.01 * x, 0.01 * y);
+        const v = p.noise(0.0001 * resolution * x, 0.0001 * resolution * y);
         const shouldBeColoredIn =
           Math.round(v * maxHeight) % heightMarker === 0;
         if (shouldBeColoredIn) {
           let white = p.color((x / W) * 255, (y / H) * 255, v * 255);
-
           p.set(x, y, white);
         }
       }
