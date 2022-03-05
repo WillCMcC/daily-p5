9018b5a40532d5f2ed899cdc2d79b2b070eea5b6
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2022-03-05 12:21:09 -0800

    topo1

--- sketches/topo.js
+++ sketches/topo.js
@@ -1,35 +1,51 @@
-import Transformer from '../utils/transformer';
-import {
-  range
-} from '../utils/utils';
+import Transformer from "../utils/transformer";
+import { range } from "../utils/utils";
 
 const Sketch = (W, H) => (p) => {
-  let { 
+  let {
     // Constants
     RADIANS,
-    PI, 
+    PI,
     HALF_PI: PI_2,
     QUARTER_PI: PI_4,
     TWO_PI,
 
     // Time
-    // Not sure why when millis() is used it errors out on _millisStart. 
+    // Not sure why when millis() is used it errors out on _millisStart.
     // A workaround is just to use p.millis()
-    year, month, day, hour, minute, second,
-  } = p
-
-  const tf = new (Transformer(p))()
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+    set,
+  } = p;
+  const maxHeight = 10000;
+  const heightMarker = 5;
+  const tf = new (Transformer(p))();
 
   p.setup = () => {
-    p.createCanvas(W, H)
-    p.angleMode(RADIANS)
-  }
+    p.createCanvas(W, H);
+    p.angleMode(RADIANS);
+    p.background(0);
+    let white = p.color(255);
 
-  p.draw = () => {
-    p.background(200)
-    p.textSize(12)
-    p.text('A template for making p5.js apps\n\nSec:'+second(), 0, H/2)
-    p.text(`${range(10)}`, 0, H/3)
-  } 
-}
-export default Sketch
+    for (let x = 0; x < W; x++) {
+      for (let y = 0; y < H; y++) {
+        const v = p.noise(x, y, 1);
+        const shouldBeColoredIn =
+          Math.round(v * maxHeight) % heightMarker === 0;
+        if (shouldBeColoredIn) {
+          let white = p.color((x / W) * 255, (y / H) * 255, v * 255);
+
+          p.set(x, y, white);
+        }
+      }
+    }
+    p.updatePixels();
+  };
+
+  p.draw = () => {};
+};
+export default Sketch;
